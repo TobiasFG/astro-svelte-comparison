34d61736499711035e161f0b98f550cdf7c9b2c2
TobiasFG <tobias.fg@gmail.com>
2025-04-11 22:59:51 +0200

    Initial setup

---
+++ sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>SvelteKit Proformance Test</title>
+    <meta
+        name="description"
+        content="A minimal SvelteKit app with SSR, Tailwind CSS, and Typescript to test performance."
+    />
+</svelte:head>
+
+{@render children()}
